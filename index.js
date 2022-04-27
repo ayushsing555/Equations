4926f4b4fa52e49c928d17b3d661ca172823142f
ayush singhal <ayushsinghalmains@gmail.com>
2022-04-27 22:55:28 +0530

    designing complete

---
+++ index.js
@@ -0,0 +1,9 @@
+const Submit = document.getElementById("Submit");
+Submit.addEventListener("click",()=>{
+    console.log("ayush");
+});
+const next = document.getElementById("nextEq");
+next.addEventListener("click",()=>{
+    console.log("ayush singhal");
+}
+)
