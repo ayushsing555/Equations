3aa2ce843ee4e9a09af3b34a88092a7e020b6069
Prachi <prachii0223@gmail.com>
2022-05-05 22:58:46 +0530

    instructions_set

---
+++ instructions.js
@@ -0,0 +1,22 @@
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  btn.setAttribute("disabled","");
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
